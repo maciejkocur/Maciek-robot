c2898949ccc4b0ee6cdb930e0668212992995f2f
Maciej Kocur <maciej_kocur@epam.com>
2016-08-24 18:55:59 +0200

    It is possible to parse a book title from empik library
    Added BookExtractor Interface responsiible for extracting BooksInfo from library.
    EmpikBookExtractor is first implemetation of it.
    BookInfo is a class for storage a book info.

---
+++ src/main/java/BookInfo.java
@@ -0,0 +1,13 @@
+public class BookInfo {
+    String title;
+
+
+    public BookInfo(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
