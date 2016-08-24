b518c44be7bdc3247c371ba4bcc4c8d7d9281b68
OlgaErmolaeva <ermololya@gmail.com>
2016-08-24 16:13:19 +0200

    set up spring configuration

---
+++ src/main/java/ja/crawler/spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package ja.crawler.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String hello(Model model) {
+
+		model.addAttribute("name", "Result of researching books");
+
+		return "index";
+	}
+}
