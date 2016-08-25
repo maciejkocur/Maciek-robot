d9a198abb712a0248b3c586841799612bfbaaaa3
OlgaErmolaeva <ermololya@gmail.com>
2016-08-25 11:53:31 +0200

    to continue work together with Lukasz on spring config created right pakcage structure, but integration with parser does not work yet

---
+++ src/main/java/ja/crawler/Entities/BookInfo.java
@@ -0,0 +1,25 @@
+package ja.crawler.Entities;
+
+public class BookInfo {
+
+
+    String title;
+
+
+    public BookInfo(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
