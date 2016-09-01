cbdc9ab272e8f6037e7bfafd2b7fada0b89d090d
Maciej Kocur <maciej_kocur@epam.com>
2016-09-01 10:40:19 +0200

    Publio bookstore working
    Added PublioParser

--- src/main/java/io/github/bookcrawler/core/impl/publio/PublioBookInfoParser.java
+++ src/main/java/io/github/bookcrawler/core/impl/publio/PublioBookInfoParser.java
@@ -4,10 +4,50 @@ import io.github.bookcrawler.core.BookInfoParser;
 import io.github.bookcrawler.core.impl.SourceScrappingResult;
 import io.github.bookcrawler.entities.BookInfo;
 import io.github.bookcrawler.entities.BookInfoBuilder;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import static io.github.bookcrawler.core.BookStore.PUBLIO;
 
 public class PublioBookInfoParser implements BookInfoParser {
+
     @Override
     public BookInfo parse(SourceScrappingResult sourceScrappingResult) {
-        return new BookInfoBuilder().author("A").title("B").description("C").library("D").price("E").url("F").build();
+        Document source = sourceScrappingResult.getSource();
+        return new BookInfoBuilder()
+                .title(parseTitle(source))
+                .author(parseAuthor(source))
+                .description(parseDescription(source))
+                .price(parsePrice(source))
+                .library(PUBLIO.toString())
+                .url(source.location())
+                .build();
+    }
+
+    private String parseTitle(Element source) {
+        return getTextByAttribute(source, "class", "product-card-title");
+    }
+
+    private String parseAuthor(Element source) {
+        return source.getElementsByAttributeValue("class", "product-card-infos")
+                .get(0)
+                .child(0)
+                .child(1)
+                .text();
+    }
+
+    private String parseDescription(Element source) {
+        return getTextByAttribute(source, "id", "product-card-publication-description");
+    }
+
+    private String parsePrice(Element source) {
+        return getTextByAttribute(source, "class", "product-card-price-promotion");
+    }
+
+    private String getTextByAttribute(Element source, String attr, String value) {
+        return source
+                .getElementsByAttributeValue(attr, value)
+                .get(0)
+                .text();
     }
 }
