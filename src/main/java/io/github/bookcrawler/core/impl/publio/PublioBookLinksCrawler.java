f5cbabbd3c66e8b368dbe9a1ba8562ee211d20b3
Maciej Kocur <maciej_kocur@epam.com>
2016-09-01 16:36:59 +0200

    Small refactor of PublioCrawler

--- src/main/java/io/github/bookcrawler/core/impl/publio/PublioBookLinksCrawler.java
+++ src/main/java/io/github/bookcrawler/core/impl/publio/PublioBookLinksCrawler.java
@@ -2,7 +2,6 @@ package io.github.bookcrawler.core.impl.publio;
 
 import io.github.bookcrawler.core.BooksLinkCrawler;
 import io.github.bookcrawler.core.SourceScrapper;
-import io.github.bookcrawler.core.impl.JsoupSourceScrapper;
 import io.github.bookcrawler.core.impl.SourceScrappingResult;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,34 +23,43 @@ public class PublioBookLinksCrawler implements BooksLinkCrawler {
             return Collections.emptyList();
         }
 
-        Integer pages = getPageQuantity(sourceScrappingResult);
-        List<String> urls = new ArrayList<>();
-
-        for (int i = 0; i < pages; i++) {
-            urls.addAll(getBookElements(sourceScrappingResult.getSource())
-                    .parallelStream()
-                    .map(e -> getLink(href(e)))
-                    .collect(Collectors.toList()));
-
-            sourceScrappingResult = sourceScrapper.scrap(getNextUrl(sourceScrappingResult));
-            if (!sourceScrappingResult.isSuccessful()) {
-                return Collections.emptyList();
-            }
-        }
-        return urls;
+        return getPageUrls(url, sourceScrapper, sourceScrappingResult)
+                .stream()
+                .map(sourceScrapper::scrap)
+                .filter(SourceScrappingResult::isSuccessful)
+                .flatMap(e -> getBookElements(e.getSource()).stream())
+                .map(e -> getLink(href(e)))
+                .collect(Collectors.toList());
     }
 
-    private String getNextUrl(SourceScrappingResult sourceScrappingResult) {
-        return getLink(sourceScrappingResult
-                .getSource()
+    private ArrayList<String> getPageUrls(String url, SourceScrapper sourceScrapper, SourceScrappingResult sourceScrappingResult) {
+        Document source = sourceScrappingResult.getSource();
+        Integer pages = getPageQuantity(source);
+
+        ArrayList<String> pageUrls = new ArrayList<>();
+        pageUrls.add(url);
+        for (int i = 1; i < pages; i++) {
+            String nextUrl = getNextUrl(source);
+            pageUrls.add(nextUrl);
+            sourceScrappingResult = sourceScrapper.scrap(nextUrl);
+            if (!sourceScrappingResult.isSuccessful()) {
+                break;
+            }
+            source = sourceScrappingResult.getSource();
+        }
+        return pageUrls;
+    }
+
+    private String getNextUrl(Document source) {
+        return getLink(source
                 .getElementsByAttributeValue("class", "pages-list")
                 .get(0)
                 .getElementsByAttributeValue("class", "page page--next")
                 .attr("href"));
     }
 
-    private Integer getPageQuantity(SourceScrappingResult sourceScrappingResult) {
-        return Integer.valueOf(sourceScrappingResult.getSource()
+    private Integer getPageQuantity(Document source) {
+        return Integer.valueOf(source
                 .getElementsByAttributeValue("class", "pages-list")
                 .get(0)
                 .children()
