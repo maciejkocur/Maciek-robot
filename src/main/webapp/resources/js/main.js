a5f50c8fa7228c35645138c73631c595d0c9ad53
clinton56 <gigo@openmailbox.org>
2016-09-02 14:38:59 +0200

    Added searching for books.
    Added search box, to search for title,
    author, etc.
    Fixed some warnings in jsp files.
    Reformated lines in code.

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,20 +1,56 @@
+// Copy-paste this code, don't ask how it works
+$(document).ready(function () {
 
-      // Copy-paste this code, don't ask how it works
-      $(document).ready(function() {
+    $('th').click(function () {
+        var table = $(this).parents('table').eq(0)
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+        if (!this.asc) {
+            rows = rows.reverse()
+        }
+        for (var i = 0; i < rows.length; i++) {
+            table.append(rows[i])
+        }
+    })
+    function comparer(index) {
+        return function (a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+        }
+    }
 
-       $('th').click(function(){
-           var table = $(this).parents('table').eq(0)
-           var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
-           this.asc = !this.asc
-           if (!this.asc){rows = rows.reverse()}
-           for (var i = 0; i < rows.length; i++){table.append(rows[i])}
-       })
-       function comparer(index) {
-           return function(a, b) {
-               var valA = getCellValue(a, index), valB = getCellValue(b, index)
-               return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
-           }
-       }
-       function getCellValue(row, index){ return $(row).children('td').eq(index).html() }
+    function getCellValue(row, index) {
+        return $(row).children('td').eq(index).html()
+    }
 
-   });
+    $(".search").keyup(function () {
+        var searchTerm = $(".search").val();
+        var listItem = $('.results tbody').children('tr');
+        var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+
+        $.extend($.expr[':'], {
+            'containsi': function (elem, i, match, array) {
+                return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+            }
+        });
+
+        $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function (e) {
+            $(this).attr('visible', 'false');
+        });
+
+        $(".results tbody tr:containsi('" + searchSplit + "')").each(function (e) {
+            $(this).attr('visible', 'true');
+        });
+
+        var jobCount = $('.results tbody tr[visible="true"]').length;
+        $('.counter').text(jobCount + ' item');
+
+        if (jobCount == '0') {
+            $('.no-result').show();
+        }
+        else {
+            $('.no-result').hide();
+        }
+    });
+
+});
