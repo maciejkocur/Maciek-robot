8ade987701efd21ee02ad849bd250bd309b835b1
OlgaErmolaeva <ermololya@gmail.com>
2016-09-02 08:42:22 +0200

    Controller was broken, it throwed NPE because of wrong mapping on index page, so fixed it

--- src/main/java/io/github/bookcrawler/controllers/DiscountFetchingController.java
+++ src/main/java/io/github/bookcrawler/controllers/DiscountFetchingController.java
@@ -1,7 +1,6 @@
 package io.github.bookcrawler.controllers;
 
 import io.github.bookcrawler.core.DiscountFetchingService;
-import io.github.bookcrawler.entities.BookInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
-import java.util.List;
 
 @Controller
 public class DiscountFetchingController {
@@ -18,17 +16,13 @@ public class DiscountFetchingController {
     DiscountFetchingService discountFetchingService;
 
     @GetMapping("/")
-    public String hello(Model model) throws IOException {
-        List<BookInfo> bookInfos = new DiscountFetchingService().getAllBooks();
-        model.addAttribute("books", bookInfos);
+    public String hello() throws IOException {
         return "index";
     }
 
     @GetMapping("/fetch/{library}")
     public String fetchBooks(@PathVariable String library, Model model) throws IOException {
-        List<BookInfo> bookInfos =discountFetchingService.getAllBooks();
-        bookInfos.addAll(discountFetchingService.getAllBooks());
-        model.addAttribute("books", bookInfos);
+        model.addAttribute("books", discountFetchingService.getAllBooks());
         return "booksResult";
     }
 }
